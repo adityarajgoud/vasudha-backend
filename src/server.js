d30f9f4b3da22f413d2de79302ac5a9df45dad46
adityarajgoud <adityagoud930@gmail.com>
2026-09-11 11:16:45 +0530

    chore: whitelist production netlify domain for CORS

--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:3000",
+  "https://vasudhafrontend.netlify.app",
   process.env.CLIENT_URL,
 ].filter(Boolean);
 
